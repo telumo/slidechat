c4cd35e8e177e82215d51d4b844c5b73c81df3e1
telumo <drumscohika@gmail.com>
2020-02-17 16:12:12 +0900

    バックエンドと連携

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 const { Elm } = require("./Main.elm");
 
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import API, { graphqlOperation } from '@aws-amplify/api';
+import { createComment } from './graphql/mutations';
+import { onCreateComment } from './graphql/subscriptions';
+
+Amplify.configure(aws_exports);
+
+
 var app = Elm.Main.init({});
-// Elm側からJSへ
-app.ports.sendMessage.subscribe(function(data) {
-  console.log(data)
-  var random = Math.floor(Math.random() * ( 80 - 20 ) + 20);
-  console.log(random + "," + random)
-  // JS側からElmへ
-  app.ports.catchMessage.send(data + "," + random)
-});
+// ElmからJSへ
+app.ports.sendMessage.subscribe(async (data) => {
+
+  const commentData = {
+    input: {
+      text: data
+    }
+  }
+  // データを送信
+  await API.graphql(graphqlOperation(createComment, commentData))
+})
+
+
+// データの購読
+API.graphql(
+  graphqlOperation(onCreateComment)
+).subscribe({
+  next: eventData => {
+    const text = eventData.value.data.onCreateComment.text
+    const random = Math.floor(Math.random() * (80 - 20) + 20)
+
+    // JSからElmへ
+    app.ports.catchMessage.send(text + "," + random)
+  },
+})
