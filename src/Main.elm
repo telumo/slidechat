d0b1d01343f274a2277d784cb3c21b256439b80e
telumo <drumscohika@gmail.com>
2020-02-22 08:28:37 +0900

    内容追加

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,9 @@ module Main exposing (..)
 
 import Slides exposing (md, mdFragments, slidesDefaultOptions, Options, Action(..))
 
-
+-- #####################
+-- タイトル
+-- #####################
 slide_title =
     Slides.md
         """
@@ -17,7 +19,23 @@ slide_pre_title =
         # はじめに
         """
 
+slide_pre_chat =
+    Slides.md
+        """
+        # チャットできます！
 
+        ぜひコメントしてください！
+
+        「分からない〜〜」とか「は？」とかなんでもいいです。
+
+        わいわい楽しくできればいいなと思っています。
+        
+        ※このスライドはElmで作られています。
+        """
+
+-- #####################
+-- 目的
+-- #####################
 slide_pre_perpose =
     Slides.mdFragments
         [ "## 勉強会の目的"
@@ -28,18 +46,23 @@ slide_pre_perpose =
         , "Elmを中心に説明しつつ、できれば関数型言語のパラダイムへの理解の手助けになればと思っています。"
         ]
 
-
+-- #####################
+-- 注意事項
+-- #####################
 slide_pre_caution =
     Slides.mdFragments
         [ "## 注意事項"
         , "- 私はElmが好きです。多少バイアスがかかった状態で話すことがあると思いますのでご注意ください。"
-        , "- 事実と私の個人的な見解がごっちゃになる場合があります。"
+        , "- 事実と私の個人的な見解がごっちゃになる場合があります。（ぜひコメントで突っ込んでください。「それはあなたの感想ですよね？」by ひろ○き）"
         , "- あらゆる技術は手段です。どんなにElmが素晴らしいと思ったとしても、使いどころを間違えると失敗します。"
+        , "- スライドは「ざっくり」です。細かいところは、公式ドキュメントや、ワークショップで説明します。"
         , "- ***Elmが好きすぎる病***が発症する可能性があります。"
         , "- ***全てがモナドに見える病***が発症する可能性はありません。（今日は、圏論やHaskellの話はしません。）"
         ]
 
-
+-- #####################
+-- 目次
+-- #####################
 slide_pre_index =
     Slides.md """
         ## 今日の内容
@@ -49,17 +72,21 @@ slide_pre_index =
         - The Elm Architecture
         - Elmでアプリを作ってみよう！
         - 発展的事項
+        - メリデメ
         - おわりに
         """
 
 
+-- #####################
+-- 始める少し前に
+-- #####################
 slide_bitpre =
     Slides.md "# 始める少し前に"
 
 
 slide_bitpre_hasegawa =
     Slides.md """
-    ## Elmと長谷川
+    ## Elmと私
     私は、Elmからフロントを書き始めたと言っても過言ではありません。
 
     もちろんElmを知る以前からHTMLやCSSやJavaScriptについては知っていましたが、何かを書いたということは特にありません。
@@ -76,12 +103,15 @@ slide_bitpre_hasegawa =
 
 slide_bitpre_question =
     Slides.mdFragments
-        [ "## 質問（認識合わせのため）"
-        , "- 関数型というパラダイムを知っていますか？使ったことがありますか？"
+        [ "## 質問（認識合わせのため / 単なる興味）"
+        , "- Elmという言語は知っていますか？"
+        , "- 純粋な関数型言語で何か書いたことがありますか？"
         , "- 関数型と聞いてどのようなことをイメージしますか？"
         ]
 
-
+-- #####################
+-- 概要
+-- #####################
 slide_overview_what =
     Slides.md """
     # Elmの概要
@@ -175,7 +205,13 @@ slide_overview_history_frp =
 
     Elmは発表当時はFRP（関数型リアクティブプログラミング）の言語として発表されました（※）
     
-    FRPと言うのは、、TODO
+    FRPと言うのは、、
+
+    - Observerパターンに代わるもの。「リスナー」「コールバック」とも。
+    - イベント駆動型ロジックをコーディングするための、合成可能なモジュール型の手法。
+    - プログラムをその入力に対する反応として表現するもの。
+
+    Reactive Extentions（Rx）をご存知でしたら、それです。
 
     ※: [My Thesis is Finally Complete! "Elm: Concurrent FRP for functional GUIs"](https://www.reddit.com/r/haskell/comments/rkyoa/my_thesis_is_finally_complete_elm_concurrent_frp/)
     
@@ -185,7 +221,7 @@ slide_overview_history_017 =
     Slides.md """
     ### FRPとバイバイした
 
-    Elmは、Elm 0.17でFRPとバイバイ（※）して、代わりにsubscriptionというメソッドを定義しました。
+    Elmは、Elm 0.17でFRPとバイバイ（※）して、代わりにsubscriptionsというメソッドを定義しました。
 
     ***Elmは時々互換性のない破壊的な変更を行います。***
 
@@ -211,13 +247,13 @@ slide_overview_history_019 =
 
 slide_overview_history_why =
     Slides.md """
-    ### Elmの歴史からみるElmの考え方
+    ### Elmの考え方
 
     ※これは個人的な意見です。
 
     Elmの考え方の根底には、
 
-    ***「あらゆる機能をまんべんなく」よりも「Webアプリに必要な*ど真ん中*を確実に」***
+    ***「あらゆる機能をまんべんなく」よりも「Webアプリに必要なものを確実に」***
 
     という考えで作られていると思います。
     そのために不要な機能はどんどん削除するのだと思います。
@@ -228,72 +264,152 @@ slide_overview_history_why =
     と[ツイート](https://twitter.com/czaplic/status/925886519511879680)しています。
     """
 
+-- #####################
+-- Elmの言語的仕様
+-- #####################
+
 slide_lang =
-    Slides.md "# Elmの言語的仕様"
-
-slide_lang_value_string =
     Slides.md """
-    # 文字列
+    # Elmの言語的仕様
 
-    文字列は一般的な言語と同様にダブルクォーテーション（`"`）で囲んで作成します。
+    ここでは、[Elmの公式サイト](https://guide.elm-lang.jp/)をもとに、
+    
+    - [言語の基礎](https://guide.elm-lang.jp/core_language.html)
+    - [型](https://guide.elm-lang.jp/types/)
 
-    文字列の連結は`++`で行います。
+    を一緒にみましょう。
+    """
 
-    ```elm
-    > "hello"
-    "hello"
+slide_lang_importance = 
+    Slides.md """
+    # 言語仕様で重要な点をまとめます
+    - Elmにおいて再代入可能な変数を定義することはできません
+    - 型（直積・直和）を自分で作成することができます
+    - パターンマッチによって型を判断してロジックを書くことができます
+    """
 
-    > "hello" ++ "world"
-    "helloworld"
+-- #####################
+-- Elmアーキテクチャ
+-- #####################
 
-    > "hello" ++ " world"
-    "hello world"
+slide_architecture =
+    Slides.md """
+    # The Elm Architecture
+
+    - [The Elm Architecture](https://guide.elm-lang.jp/architecture/)
+
+    を一緒にみましょう。
+    """
+
+slide_architecture_importance = 
+    Slides.md
+    """
+    ## The Elm Architectureで重要な点をまとめます
+    - Model, Update, Viewから構成される
+    - （Viewから）Msgが発火される
+    - Updateは、Msgを受け取ってModelを変える
+    - Modelを元にしてViewは作成
+    """
+
+-- #####################
+-- ワークショップ
+-- #####################
+slide_workshop = 
+    Slides.md """
+    # Elmでアプリを作ろう！
+
+    ここでは実際に手を動かしながら実装していきます。
+
+    みなさんもローカルにelmをインストールするか、
+
     ```
+    npm install -g elm
+    ```
+
+    [Ellie](https://ellie-app.com/)でやってみてください！
 
     """
 
-slide_lang_value_number =
+-- #####################
+-- 発展的事項
+-- #####################
+
+slide_next =
     Slides.md """
-    # 数値
+    # 発展的事項
 
-    数値も一般的な言語と同様に利用できます。つまらないですね。
+    ※ 時間があれば話します。
 
-    ```elm
-    > 2 + 3 * 4
-    14
+    フロントに関係する話
+    - JSとの連携方法（port）
+    - CSS in Elm
+    
+    関数型に関係する話
+    - 関数（カリー化、部分適応）
+    - 代数的データ型
+    """
 
-    > (2 + 3) * 4
-    20
+slide_next_port = 
+    Slides.md """
+    # JSとの連携方法（port）
+    """
 
-    > 9 / 2
-    4.5
+-- #####################
+-- メリデメ
+-- #####################
 
-    > 9 // 2
-    4
-    ```
+slide_merit_demerit =
+    Slides.md """
+    # メリデメ
+    """
+slide_merit = 
+    Slides.md """
+    # メリット（いいところ）
+    - コミュニティーが狭い => 何かしらコミュニティーにいくと界隈のすごい人にあえる（みんな優しい）
+    - 教育的にいいのかも？ => Elm自体は簡単
+    - 関数型に慣れる最初に
+    - コミットしほうだい
+    """
+
+slide_demerit = 
+    Slides.md """
+    # デメリット（だめなところ）
+    - 結局JS使う必要が出てくる場合がある => 「Elmじゃなくても」ってなる
+    - エコシステムが弱い
+    - ***バージョンアップ時に、破壊的変更が行われることが多い。*** => これな。
+    """
+
+slide_when_to_use = 
+    Slides.md """
+    # 使いどころ
+
+    > SEOや動作速度を気にしないツール系（管理画面）が得意
+
+    らしいので、管理画面から作ってみるのもいいかも。
 
     """
 
-slide_lang_value_function_1 =
+-- #####################
+-- おわりに
+-- #####################
+
+slide_last =
     Slides.md """
-    # 関数
+    # おわりに
+    """
 
-    関数の利用方法を詳しく見てみましょう。
+slide_last_onegai =
+    Slides.md """
+    最も学ぶべきでない言語2019の優勝者Elmですが、
 
-    ```elm
-    > isNegative n = n < 0
-    <function>
+    言語自体は素晴らしい言語だと思います。
 
-    > isNegative 4
-    False
+    関数型の勉強になりますし、ぜひ一度使ってみてください！
+    """
 
-    > isNegative -71                                                                    
-    True
-
-    > isNegative (-3 * -4)
-    False
-    ```
-
+slide_last_reference =
+    Slides.md """
+    ## 参考URL
     """
 
 myOption : Options
@@ -328,6 +444,7 @@ main =
         myOption
         [ slide_title
         , slide_pre_title
+        , slide_pre_chat
         , slide_pre_perpose
         , slide_pre_caution
         , slide_pre_index
@@ -347,7 +464,16 @@ main =
         , slide_overview_history_019
         , slide_overview_history_why
         , slide_lang
-        , slide_lang_value_string
-        , slide_lang_value_number
-        , slide_lang_value_function_1
+        , slide_lang_importance
+        , slide_architecture 
+        , slide_architecture_importance
+        , slide_workshop
+        , slide_next
+        , slide_merit_demerit
+        , slide_merit
+        , slide_demerit
+        , slide_when_to_use
+        , slide_last
+        , slide_last_onegai
+        , slide_last_reference
         ]
